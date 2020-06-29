c062935efa44a9b6a6bf159a10d77686d45c4192
Glen <mistergjones@yahoo.com.au>
2020-06-29 23:18:45 +1000

    Updates

---
+++ precache-manifest.a4925c7655b6b4b6d72efa6820fba2d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21959515123c1019c9a9ccb31a4ce7a1",
+    "url": "/taskTimeBoxer/index.html"
+  },
+  {
+    "revision": "a565f7d5da8303af1564",
+    "url": "/taskTimeBoxer/static/js/2.5b6e9274.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/taskTimeBoxer/static/js/2.5b6e9274.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82d2fe98030c307993be",
+    "url": "/taskTimeBoxer/static/js/main.9d03c6fd.chunk.js"
+  },
+  {
+    "revision": "1ade6871dc5056e97beb",
+    "url": "/taskTimeBoxer/static/js/runtime-main.42e6059b.js"
+  }
+]);
